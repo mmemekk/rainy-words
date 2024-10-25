a39a9de83b76e621c2014a7653d35c64847a1acc
Thanakron Meelarp <fastturtlemigration@nocnoc.local>
2024-10-25 15:54:23 +0700

    draftonegamepage

--- clients/src/page/Gamepage.jsx
+++ clients/src/page/Gamepage.jsx
@@ -21,13 +21,26 @@ const Game = ()=>{
                 { text: word, id: Date.now() } // Use timestamp as a unique id
             ]);
         });
+        socket.on('counter', (time) => {
+            setTimer(time); // Update the timer state with the received time
+        });
+    }, []);
 
-    }, []); 
+    const navigate = useNavigate();
+
+    function retoprevious(){
+        navigate('/lobby');
+    }
 
 
     return (
         <div className="game-container">
-            <h1>Rainy Word Game</h1>
+            <button onClick={retoprevious}>RE</button>
+            <div className="topBar">
+                <p>Player1 Player2</p>
+                <div className='clock'>Time</div>
+                <p>Plater3 Player4</p>
+            </div>
             <div className="falling-words">
                 {fallingWords.map((word) => (
                     <div key={word.id} className="falling-word">{word.text}</div>
@@ -35,7 +48,7 @@ const Game = ()=>{
             </div>
         </div>
     );
- 
+
 }
 
 export default Game;
